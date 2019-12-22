910c7339c6248ab32b67ffecea4b1ba1a54e4b33
Aslak Hollund <aslakhol@gmail.com>
2019-12-22 01:49:34 +0100

    refactor timer

--- script.js
+++ script.js
@@ -1,50 +1,23 @@
 import { selectStylesheet } from "./js/buttons.js";
 import { toggleTimerHighlight } from "./js/highlight.js";
-import { setAnimationOn, setAnimationOff } from "./js/animation.js";
-import { setTransitionIn, setTransitionOut } from "./js/transition.js";
-import { setBorderWidthOn, setBorderWidthOff } from "./js/border-width.js";
-
-const minutesDisplay = document.querySelector("#minutes");
-const secondsDisplay = document.querySelector("#seconds");
-const tenthsDisplay = document.querySelector("#tenths");
+import { startTimer, stopTimer } from "./js/timer.js";
 
 let timerRunning = false;
 let interval;
 
-const updateTimer = startTime => {
-  const currentTime = Date.now();
-  updateDisplay(currentTime - startTime);
+const toggleTimer = () => {
+  if (timerRunning) {
+    stopTimer(interval);
+    timerRunning = false;
+  } else {
+    interval = startTimer();
+    timerRunning = true;
+  }
 };
 
-const updateDisplay = elapsedTime => {
-  writeToSpans(separateUnits(elapsedTime));
+const buttonPress = buttonName => {
+  selectStylesheet(buttonName);
 };
-
-const separateUnits = elapsedTime => {
-  const units = splitToUnits(elapsedTime);
-  return units.map(unit => prepareDoubleDigitString(unit));
-};
-
-const splitToUnits = ms => {
-  const minutes = Math.trunc(ms / 60000);
-  ms -= minutes * 60000;
-  const seconds = Math.trunc(ms / 1000);
-  ms -= seconds * 1000;
-  const tenths = Math.trunc((ms % 1000) / 10);
-  return [minutes, seconds, tenths];
-};
-
-const prepareDoubleDigitString = num =>
-  `${num}`.length === 1 ? `0${num}` : `${num}`;
-
-const writeToSpans = units => {
-  const [minutes, seconds, tenths] = units;
-  minutesDisplay.innerHTML = minutes;
-  secondsDisplay.innerHTML = seconds;
-  tenthsDisplay.innerHTML = tenths;
-};
-
-// Control related
 
 document.addEventListener("click", e => {
   if (!document.querySelector(".buttons").contains(e.target)) {
@@ -60,29 +33,4 @@ document.onkeypress = e => {
   } else if (e.code === "Enter") {
     toggleTimerHighlight();
   }
-};
-
-const toggleTimer = () => {
-  timerRunning ? stopTimer() : startTimer();
-};
-
-const stopTimer = () => {
-  clearInterval(interval);
-  timerRunning = false;
-  setBorderWidthOff();
-  setTransitionOut();
-  setAnimationOff();
-};
-
-const startTimer = () => {
-  const startTime = Date.now();
-  interval = setInterval(() => updateTimer(startTime), 10);
-  timerRunning = true;
-  setBorderWidthOn();
-  setTransitionIn();
-  setAnimationOn();
-};
-
-const buttonPress = buttonName => {
-  selectStylesheet(buttonName);
 };
